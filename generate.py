6154da00decb9966683c5ee6ed3cf2d2ad4f31a7
Samuel J Pope <sjpope03@gmail.com>
2025-02-18 22:18:03 -0600

    Added fixed width file parsing logic. Logical error in whitespace interpretation causing bug

--- generate.py
+++ generate.py
@@ -1,20 +1,17 @@
 import json
 import random
 import string
+import csv
 
 def generate_string(offset):
     length = random.randint(1, offset)
     rand_str = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
     return rand_str.ljust(offset)[:offset]
 
-def generate(outfile = "fixed.txt", n=15):
-    
-    with open("spec.json", "r") as file:
-        spec = json.load(file)
+def generate(spec, outfile = "fixed.txt", n=15):
     
     names = spec["ColumnNames"]
     offsets = list(map(int, spec["Offsets"]))
-    
     
     with open(outfile, "w", encoding=spec["FixedWidthEncoding"]) as data:
 
@@ -37,5 +34,38 @@ def generate(outfile = "fixed.txt", n=15):
     
     print(f"Prob 1 File Generation Complete")
 
+def parse(spec):
+    
+    names = spec["ColumnNames"]
+    offsets = list(map(int, spec["Offsets"]))
+    
+    # Parse fixed width to generate delimited file
+    with open("fixed.txt", "r") as infile, open("parsed.csv", "w", encoding=spec["DelimitedEncoding"]) as outfile:
+        
+        writer = csv.DictWriter(outfile, names)
+        
+        for line in infile:
+            i = 0
+            row = {}
+            
+            for name, offset in zip(names, offsets):
+                row[name] = line[i:offset].rstrip()
+                i += offset
+            
+            # map folumn nam,es to values for row
+            
+            writer.writerow(row)
+        
+             
+    print('Prob 1 Complete: parsed.csv generated successfully')
+        
+        
+
+
 if __name__ == "__main__":
-    generate()
+    
+    with open("spec.json", "r") as file:
+        spec = json.load(file)
+    
+    generate(spec)
+    parse(spec)
